fec837e96d8b04c618cbfef701b4d5f6a222402a
naveenluke <naveen.luke@gmail.com>
2020-07-17 08:31:22 +0530

    Update varmake.sh

--- varmake.sh
+++ varmake.sh
@@ -17,8 +17,8 @@ fi
 # add variant_effect_predictor to path, contains line at the top #!/usr/bin/env perl to allow for execution
 variant_effect_predictor.pl -i data/${FILE}${OTHERNAME}_vt.vcf --cache --sift b --polyphen b --symbol --numbers --biotype --total_length --allele_number -o data/${FILE}${OTHERNAME}-vep-vt.vcf --vcf --fields ALLELE,Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ALLELE_NUM,cDNA_position --offline --fork 12 --force_overwrite
 
-rm data/${FILE}${OTHERNAME}_vt.vcf
+#rm data/${FILE}${OTHERNAME}_vt.vcf
 
 cat <(grep "^#" data/${FILE}${OTHERNAME}-vep-vt.vcf) <(grep -v "^#" data/${FILE}${OTHERNAME}-vep-vt.vcf | sort -k1,1 -k2,2n) | bgzip -c > data/${FILE}${OTHERNAME}-vep-vt.vcf.gz; tabix -f data/${FILE}${OTHERNAME}-vep-vt.vcf.gz
 
-rm data/${FILE}${OTHERNAME}-vep-vt.vcf
+#rm data/${FILE}${OTHERNAME}-vep-vt.vcf
